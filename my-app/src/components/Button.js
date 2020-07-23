d6bea1982319d3f1c63050caddfd1865991d43fd
David <david.he07@yahoo.ca>
2020-07-23 17:57:05 -0400

    created show more button in storybook

---
+++ my-app/src/components/Button.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Button.scss";
+
+const classNames = require("classnames");
+
+export default function Button(props) {
+  //function renders a button element
+  let buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    // "button--danger": props.danger,
+  });
+
+  return (
+    <button //uses props.children value as the button text
+      className={buttonClass} //button element is wrapped in Button component
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+}
